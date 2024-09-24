41e9343fa90e85ba77943a7514e604b400465d5f
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-25 01:45:08 +0900

    Docs: 회원가입/로그인, 토큰재발급 응답 DTO Swagger 작성

--- src/main/java/cjkimhello97/toy/crashMyServer/auth/service/dto/SignupRequest.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/service/dto/SignupRequest.java
@@ -1,7 +1,12 @@
 package cjkimhello97.toy.crashMyServer.auth.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "[ HTTP ] SignupRequest : 회원가입 요청 DTO")
 public record SignupRequest(
+        @Schema(description = "aaa")
         String nickname,
+        @Schema(description = "1234")
         String password
 ) {
 
