bc67db0041542e5a842eb9c3bd27e801cc7ad6ba
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 20:21:37 +0900

    Feat: 회원가입/로그인/토큰재발급 기능의 AuthService 구현

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/service/AuthService.java
@@ -0,0 +1,74 @@
+package cjkimhello97.toy.crashMyServer.auth.service;
+
+import static cjkimhello97.toy.crashMyServer.auth.exception.AuthExceptionType.*;
+
+import cjkimhello97.toy.crashMyServer.auth.controller.dto.SigninResponse;
+import cjkimhello97.toy.crashMyServer.auth.controller.dto.TokenResponse;
+import cjkimhello97.toy.crashMyServer.auth.exception.AuthException;
+import cjkimhello97.toy.crashMyServer.auth.infrastructure.JwtProvider;
+import cjkimhello97.toy.crashMyServer.auth.service.dto.SignupRequest;
+import cjkimhello97.toy.crashMyServer.member.domain.Member;
+import cjkimhello97.toy.crashMyServer.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+    private final RedisTokenService redisTokenService;
+
+    @Transactional
+    public SigninResponse signUp(SignupRequest signUpRequest) {
+        String nickname = signUpRequest.nickname();
+        String password = signUpRequest.password();
+        // 닉네임 존재 O = 로그인 로직
+        if (memberRepository.findByNickname(nickname).isPresent()) {
+            return signIn(nickname, password);
+        }
+        // 닉네임 존재 X = 회원가입 후 로그인 로직
+        Member member = Member.builder()
+                .nickname(nickname)
+                .password(passwordEncoder.encode(password))
+                .build();
+        memberRepository.save(member);
+
+        return signIn(nickname, password);
+    }
+
+    public SigninResponse signIn(String nickname, String password) {
+        Member member = memberRepository.findByNickname(nickname).get();
+        String savedNickname = member.getNickname();
+        String savedPassword = member.getPassword();
+        // 닉네임 존재 && 비밀번호 존재 X = 예외
+        if (savedNickname.equals(savedNickname) && !passwordEncoder.matches(password, savedPassword)) {
+            throw new AuthException(WRONG_PASSWORD);
+        }
+        // 닉네임 존재 && 비밀번호 존재 O = 로그인
+        String accessToken = jwtProvider.createAccessToken(member.getMemberId());
+        String refreshToken = jwtProvider.createRefreshToken(member.getMemberId());
+        return new SigninResponse(accessToken, refreshToken);
+    }
+
+    @Transactional
+    public TokenResponse reissueTokens(Long memberId, String refreshToken) {
+        String storedRefreshToken = redisTokenService.getRefreshToken(String.valueOf(memberId));
+
+        if (!storedRefreshToken.equals(refreshToken)) {
+            throw new AuthException(INVALID_TOKEN);
+        }
+
+        String newAccessToken = jwtProvider.createAccessToken(memberId);
+        String newRefreshToken = jwtProvider.createRefreshToken(memberId);
+        redisTokenService.setRefreshToken(memberId, newRefreshToken);
+
+        return new TokenResponse(newAccessToken, newRefreshToken);
+    }
+}
