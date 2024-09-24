50df2bb89fea2d976771b205add9073736cc38d7
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 21:28:20 +0900

    Feat: 클릭 횟수 소수점 제거하는 포매터 유틸 클래스 구현

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/click/utils/CountFormatter.java
@@ -0,0 +1,8 @@
+package cjkimhello97.toy.crashMyServer.click.utils;
+
+public class CountFormatter {
+
+    public static String format(Double count) {
+        return String.format("%.0f", count);
+    }
+}
