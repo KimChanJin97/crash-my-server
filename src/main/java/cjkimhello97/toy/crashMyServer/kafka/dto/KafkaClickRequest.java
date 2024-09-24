8e64fd10e8ff07432d818e613107edb66350d97c
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 20:52:53 +0900

    Feat: 카프카 브로커에게 클릭 횟수를 전달하기 위한 DTO 구현

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/kafka/dto/KafkaClickRequest.java
@@ -0,0 +1,17 @@
+package cjkimhello97.toy.crashMyServer.kafka.dto;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KafkaClickRequest implements Serializable {
+
+    private String nickname;
+    private String count;
+}
