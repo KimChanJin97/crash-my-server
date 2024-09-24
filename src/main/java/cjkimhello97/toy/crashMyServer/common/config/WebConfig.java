31af608bdf8bdc8fe5a7338f9afafe984d987519
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 20:23:18 +0900

    Feat: CorsFilter 빈등록

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/common/config/WebConfig.java
@@ -0,0 +1,21 @@
+package cjkimhello97.toy.crashMyServer.common.config;
+
+import cjkimhello97.toy.crashMyServer.common.filter.CorsFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<Filter> addCorsMappings() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new CorsFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
