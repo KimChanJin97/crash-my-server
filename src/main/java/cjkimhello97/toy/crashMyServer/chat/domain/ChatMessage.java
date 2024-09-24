0aa645c73da3ba638e2fdc7b141a46a7019415b4
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 20:31:42 +0900

    Feat: 채팅 메시지 엔티티 ChatMessage 구현

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/chat/domain/ChatMessage.java
@@ -0,0 +1,47 @@
+package cjkimhello97.toy.crashMyServer.chat.domain;
+
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Document(collection = "chatMessage")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage {
+
+    @Id
+    private String id;
+
+    private Long chatRoomId;
+
+    private Long senderId;
+
+    private String senderNickname;
+
+    private Long receiverId;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @Builder
+    protected ChatMessage(
+            Long senderId,
+            String senderNickname,
+            Long receiverId,
+            String content,
+            Long chatRoomId,
+            LocalDateTime createdAt
+    ) {
+        this.senderId = senderId;
+        this.senderNickname = senderNickname;
+        this.receiverId = receiverId;
+        this.content = content;
+        this.chatRoomId = chatRoomId;
+        this.createdAt = createdAt;
+    }
+}
