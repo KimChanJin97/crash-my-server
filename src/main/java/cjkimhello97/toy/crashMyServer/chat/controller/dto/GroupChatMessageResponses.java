44849c1a36a588a390d8f1558027673c0c1abfa1
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 21:18:07 +0900

    Feat: 채팅방 내 채팅 메시지 응답 리스트 DTO 구현

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/chat/controller/dto/GroupChatMessageResponses.java
@@ -0,0 +1,16 @@
+package cjkimhello97.toy.crashMyServer.chat.controller.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupChatMessageResponses {
+
+    List<GroupChatMessageResponse> groupChatMessageResponses;
+}
