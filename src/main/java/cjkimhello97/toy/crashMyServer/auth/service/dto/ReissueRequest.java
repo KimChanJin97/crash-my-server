546f92813f41ac50ddb285e54bcfaca5bb40d11f
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-24 20:17:48 +0900

    Feat: 회원가입/로그인 요청 DTO, 토큰재발급 요청 DTO 구현

---
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/service/dto/ReissueRequest.java
@@ -0,0 +1,11 @@
+package cjkimhello97.toy.crashMyServer.auth.service.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "[ HTTP ] ReissueRequest : 토큰 재발급 요청 DTO")
+public record ReissueRequest(
+        @Schema(defaultValue = "리프레시 토큰", example = "aaa.bbb.ccc")
+        String refreshToken
+) {
+
+}
---
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/service/dto/SignupRequest.java
@@ -0,0 +1,8 @@
+package cjkimhello97.toy.crashMyServer.auth.service.dto;
+
+public record SignupRequest(
+        String nickname,
+        String password
+) {
+
+}
