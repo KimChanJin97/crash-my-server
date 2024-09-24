12a02929c6c891b9e87bc818581cef294b9d1876
KimChanJin97 <cjkimhello97@gmail.com>
2024-09-25 01:43:57 +0900

    Docs: 회원가입/로그인, 토큰재발급 요청 DTO Swagger 작성

--- src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/SigninResponse.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/SigninResponse.java
@@ -1,7 +1,12 @@
 package cjkimhello97.toy.crashMyServer.auth.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "[ HTTP ] SigninResponse : 로그인 응답 DTO")
 public record SigninResponse(
+        @Schema(defaultValue = "액세스 토큰", example = "aaa.bbb.ccc")
         String accessToken,
+        @Schema(defaultValue = "리프레시 토큰", example = "aaa.bbb.ccc")
         String refreshToken
 ) {
 
--- src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/TokenResponse.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/TokenResponse.java
@@ -1,7 +1,12 @@
 package cjkimhello97.toy.crashMyServer.auth.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "[ HTTP ] TokenResponse : 토큰 재발급 응답 DTO")
 public record TokenResponse(
+        @Schema(defaultValue = "액세스 토큰", example = "aaa.bbb.ccc")
         String accessToken,
+        @Schema(defaultValue = "리프레시 토큰", example = "aaa.bbb.ccc")
         String refreshToken
 ) {
 
