df9bf6b36e522f65d32e4b66e7a6862d997f9112
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-03 22:58:05 +0900

    Config: Swagger 요청 URL 추가

--- src/main/java/cjkimhello97/toy/crashMyServer/common/config/SwaggerConfig.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/common/config/SwaggerConfig.java
@@ -2,10 +2,6 @@ package cjkimhello97.toy.crashMyServer.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -18,10 +14,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        Server server = new Server();
-        server.setUrl("https://crash-my-server.site");
-        return new OpenAPI().servers(List.of(server));
-    }
 }
