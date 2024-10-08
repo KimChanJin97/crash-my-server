7975e646efc4617529278c03f0acaa3238484dd7
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-09 03:21:27 +0900

    Fix: 카프카 컨슈머 설정, 토픽 설정 수정

--- src/main/java/cjkimhello97/toy/crashMyServer/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/kafka/config/KafkaConsumerConfig.java
@@ -1,7 +1,11 @@
 package cjkimhello97.toy.crashMyServer.kafka.config;
 
+import static org.springframework.kafka.listener.ContainerProperties.AckMode.*;
+
 import cjkimhello97.toy.crashMyServer.kafka.constant.KafkaConstants;
 import cjkimhello97.toy.crashMyServer.kafka.dto.KafkaChatMessageRequest;
+import cjkimhello97.toy.crashMyServer.kafka.dto.KafkaClickRankRequest;
+import cjkimhello97.toy.crashMyServer.kafka.dto.KafkaClickRequest;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties.AckMode;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @EnableKafka
@@ -23,8 +28,59 @@ public class KafkaConsumerConfig {
     private final KafkaConstants kafkaConstants;
 
     @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaClickRequest> kafkaClickRequestConcurrentKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaClickRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.getContainerProperties().setAckMode(MANUAL);
+        factory.setConsumerFactory(kafkaClickRequestConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, KafkaClickRequest> kafkaClickRequestConsumerFactory() {
+        Map<String, Object> consumerProps = new HashMap<>();
+
+        JsonDeserializer<KafkaClickRequest> deserializer = new JsonDeserializer<>(KafkaClickRequest.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConstants.KAFKA_BROKER);
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // 오토 커밋 false
+        return new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaClickRankRequest> kafkaClickRankRequestConcurrentKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaClickRankRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.getContainerProperties().setAckMode(MANUAL);
+        factory.setConsumerFactory(kafkaClickRankRequestConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, KafkaClickRankRequest> kafkaClickRankRequestConsumerFactory() {
+        Map<String, Object> consumerProps = new HashMap<>();
+
+        JsonDeserializer<KafkaClickRankRequest> deserializer = new JsonDeserializer<>(KafkaClickRankRequest.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConstants.KAFKA_BROKER);
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // 오토 커밋 false
+        return new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, KafkaChatMessageRequest> kafkaChatMessageRequestConcurrentKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, KafkaChatMessageRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.getContainerProperties().setAckMode(MANUAL);
         factory.setConsumerFactory(kafkaChatMessageRequestConsumerFactory());
         return factory;
     }
@@ -42,6 +98,7 @@ public class KafkaConsumerConfig {
         consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
         consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // 오토 커밋 false
         return new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), deserializer);
     }
 }
