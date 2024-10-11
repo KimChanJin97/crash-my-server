e005516aa45bbbeed8d458af48a86ef145e81379
KimChanJin97 <cjkimhello97@gmail.com>
2024-10-11 10:26:37 +0900

    Style: DTO 이름 카멜 케이스로 수정

--- src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/SignInResponse.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/SignInResponse.java
@@ -3,7 +3,7 @@ package cjkimhello97.toy.crashMyServer.auth.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(title = "[ HTTP ] SigninResponse : 로그인 응답 DTO")
-public record SigninResponse(
+public record SignInResponse(
         String accessToken,
         String refreshToken
 ) {
