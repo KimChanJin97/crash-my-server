a345751abcc7645e345d8da9ebd3d7dcbe62c165
KimChanJin97 <cjkimhello97@gmail.com>
2024-12-19 19:51:35 +0900

    Refactor: Redis Key String -> Long 변경으로 인한 리포지토리, 서비스 수정

--- src/main/java/cjkimhello97/toy/crashMyServer/redis/repository/RefreshTokenRepository.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/redis/repository/RefreshTokenRepository.java
@@ -3,6 +3,6 @@ package cjkimhello97.toy.crashMyServer.redis.repository;
 import cjkimhello97.toy.crashMyServer.redis.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
 
 }
