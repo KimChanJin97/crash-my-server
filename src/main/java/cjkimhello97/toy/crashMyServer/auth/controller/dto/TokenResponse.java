03d3121f603cad25d8a6c52b06202d79979b2e56
KimChanJin97 <cjkimhello97@gmail.com>
2024-12-19 18:53:45 +0900

    Feat: 인증/인가 DTO 필드 수정

--- src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/TokenResponse.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/auth/controller/dto/TokenResponse.java
@@ -1,11 +1,13 @@
 package cjkimhello97.toy.crashMyServer.auth.controller.dto;
 
+import cjkimhello97.toy.crashMyServer.redis.domain.AccessToken;
+import cjkimhello97.toy.crashMyServer.redis.domain.RefreshToken;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(title = "[ HTTP ] TokenResponse : 토큰 재발급 응답 DTO")
 public record TokenResponse(
-        String accessToken,
-        String refreshToken
+        AccessToken accessToken,
+        RefreshToken refreshToken
 ) {
 
 }
