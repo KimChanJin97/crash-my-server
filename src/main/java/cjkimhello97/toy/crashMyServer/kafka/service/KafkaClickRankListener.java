1d475aec02d956593dd48d194d210ea40f00030e
KimChanJin97 <cjkimhello97@gmail.com>
2025-01-03 12:34:52 +0900

    Fix: 처리한 적이 있는 메시지는 처리하지 않고 커밋하도록 수정

--- src/main/java/cjkimhello97/toy/crashMyServer/kafka/service/KafkaClickRankListener.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/kafka/service/KafkaClickRankListener.java
@@ -1,16 +1,12 @@
 package cjkimhello97.toy.crashMyServer.kafka.service;
 
-import static cjkimhello97.toy.crashMyServer.kafka.exception.ProcessedKafkaRequestExceptionType.ALREADY_PROCESSED_MESSAGE;
-
-import cjkimhello97.toy.crashMyServer.kafka.domain.ProcessedKafkaRequest;
 import cjkimhello97.toy.crashMyServer.click.dto.KafkaClickRankRequest;
-import cjkimhello97.toy.crashMyServer.kafka.exception.ProcessedKafkaRequestException;
+import cjkimhello97.toy.crashMyServer.kafka.domain.ProcessedKafkaRequest;
 import cjkimhello97.toy.crashMyServer.kafka.repository.ProcessedKafkaRequestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class KafkaClickRankListener {
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private final KafkaRequestSender kafkaRequestSender;
     private final ProcessedKafkaRequestRepository processedKafkaRequestRepository;
 
     @Transactional
     @KafkaListener(
-            id = "clickRankListener0",
             groupId = "clickRankListener",
             topics = "click-rank",
-            containerFactory = "kafkaClickRankRequestConcurrentKafkaListenerContainerFactory"
+            containerFactory = "kafkaClickRankRequestContainerFactory"
     )
     public void listenClickRankTopic(
             KafkaClickRankRequest request,
@@ -36,12 +31,14 @@ public class KafkaClickRankListener {
         // 이미 처리한 적이 있는 메시지라면 예외 발생
         String uuid = request.getUuid();
         if (processedKafkaRequestRepository.existsByUuid(uuid)) {
-            throw new ProcessedKafkaRequestException(ALREADY_PROCESSED_MESSAGE);
+            acknowledgment.acknowledge();
+            return;
         }
-        // 메시지 처리(전송)
-        messagingTemplate.convertAndSend("/sub/click-rank", request);
+        // 처리한 적이 없던 메시지이므로 처리
+        kafkaRequestSender.convertAndSend("/sub/click-rank", request);
         // 처리했다면 처리했음을 기록(저장)
         processedKafkaRequestRepository.save(new ProcessedKafkaRequest(uuid));
+        // 처리 및 저장했다면 커밋
         acknowledgment.acknowledge();
 
     }
--- src/main/java/cjkimhello97/toy/crashMyServer/kafka/service/KafkaEnterListener.java
+++ src/main/java/cjkimhello97/toy/crashMyServer/kafka/service/KafkaEnterListener.java
@@ -1,16 +1,12 @@
 package cjkimhello97.toy.crashMyServer.kafka.service;
 
-import static cjkimhello97.toy.crashMyServer.kafka.exception.ProcessedKafkaRequestExceptionType.ALREADY_PROCESSED_MESSAGE;
-
 import cjkimhello97.toy.crashMyServer.chat.dto.KafkaChatMessageRequest;
 import cjkimhello97.toy.crashMyServer.kafka.domain.ProcessedKafkaRequest;
-import cjkimhello97.toy.crashMyServer.kafka.exception.ProcessedKafkaRequestException;
 import cjkimhello97.toy.crashMyServer.kafka.repository.ProcessedKafkaRequestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,30 +15,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class KafkaEnterListener {
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private final KafkaRequestSender kafkaRequestSender;
     private final ProcessedKafkaRequestRepository processedKafkaRequestRepository;
 
     @Transactional
     @KafkaListener(
-            id = "enterListener0",
             groupId = "enterListener",
             topics = "enter",
-            containerFactory = "kafkaChatMessageRequestConcurrentKafkaListenerContainerFactory"
+            containerFactory = "kafkaChatMessageRequestContainerFactory"
     )
     public void listenEnterTopic(
             KafkaChatMessageRequest request,
             Acknowledgment acknowledgment
     ) {
-        // 이미 처리한 적이 있는 메시지라면 예외 발생
         String uuid = request.getUuid();
+        // 처리한 적이 있던 메시지이므로 커밋
         if (processedKafkaRequestRepository.existsByUuid(uuid)) {
-            throw new ProcessedKafkaRequestException(ALREADY_PROCESSED_MESSAGE);
+            acknowledgment.acknowledge();
+            return;
         }
-        // 메시지 처리(전송)
+        // 처리한 적이 없던 메시지이므로 처리
         Long chatRoomId = request.getChatRoomId();
-        messagingTemplate.convertAndSend("/sub/enter" + chatRoomId, request);
+        kafkaRequestSender.convertAndSend("/sub/enter/" + chatRoomId, request);
         // 처리했다면 처리했음을 기록(저장)
         processedKafkaRequestRepository.save(new ProcessedKafkaRequest(uuid));
+        // 처리 및 저장했다면 커밋
         acknowledgment.acknowledge();
     }
 }
